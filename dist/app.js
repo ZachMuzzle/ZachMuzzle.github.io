4593f859fbc64eaeb77d1612423294e2fad1f87a
Zachary Muzzleman <zach12342@me.com>
2024-12-02 20:50:56 -0500

    Updated README

--- dist/app.js
+++ dist/app.js
@@ -6,10 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const index_1 = require("./routes/index");
-const aboutMe_1 = require("./routes/aboutMe");
-const resume_1 = require("./routes/resume");
-const projects_1 = require("./routes/projects");
-const contact_1 = require("./routes/contact");
 const app = (0, express_1.default)();
 // Middleware to parse JSON
 app.use(express_1.default.json());
@@ -20,10 +16,11 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 // Use the routes
 app.use('/', index_1.router);
-app.use('/about-me', aboutMe_1.router);
-app.use('/resume', resume_1.router);
-app.use('/projects', projects_1.router);
-app.use('/contact', contact_1.router);
+// Routes not needed unless we deploy backend to another service.
+// app.use('/about-me', aboutMeRouter);
+// app.use('/resume', resumeRouter);
+// app.use('/projects', projectsRouter);
+// app.use('/contact', contactRouter);
 // Catch-all route for 404
 app.use((req, res) => {
     res.status(404).send('Page not found');
--- public/js/removeHash.js
+++ public/js/removeHash.js
@@ -1,26 +1,34 @@
 "use strict";
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        var _a;
+/*
+* Hashing from frontend not needed when only using github pages since backend isn't used
+* Will need to deploy backend to another server
+
+document.querySelectorAll<HTMLAnchorElement>('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (this: HTMLAnchorElement, e: MouseEvent) {
         e.preventDefault();
-        const targetId = (_a = this.getAttribute('href')) === null || _a === void 0 ? void 0 : _a.substring(1); // handle possible null
+        const targetId = this.getAttribute('href')?.substring(1); // handle possible null
+
         if (targetId) {
             const targetElement = document.getElementById(targetId);
+
             if (targetElement) {
                 targetElement.scrollIntoView({ behavior: 'smooth' });
+
                 // Update URL without the hash
                 window.history.pushState({}, '', `${targetId}`); // Correctly formatted as suggested
             }
         }
     });
 });
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentUrl = window.location.href;
     const hashIndex = currentUrl.indexOf("#");
-    if (hashIndex !== -1) {
+    if(hashIndex !== -1) {
         // Construct a new URL by removing only the "#" character
-        const newUrl = currentUrl.slice(0, hashIndex) + currentUrl.slice(hashIndex + 1);
+        const newUrl = currentUrl.slice(0, hashIndex) + currentUrl.slice(hashIndex+1);
         // Using the History API to update the URL without refreshing the page
         history.pushState({}, document.title, newUrl);
     }
 });
+ */ 
