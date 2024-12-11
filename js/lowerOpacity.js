581d8265928ab36f387a7acf7388a1d3cc6cca0b
ZachMuzzle <ZachMuzzle@users.noreply.github.com>
2024-12-11 20:03:10 +0000

    deploy: 31e8927ca118efa066df66097a57c17960a0fc95

---
+++ js/lowerOpacity.js
@@ -0,0 +1,24 @@
+"use strict";
+function lowerBackgroundColor(querySelector) {
+    try {
+        const colorItem = document.querySelector(`.${querySelector}`);
+        window.addEventListener('scroll', () => {
+            /* Calculate scroll position relative to the viewport height */
+            const scrollTop = window.scrollY;
+            const maxScroll = window.innerHeight;
+            /* Calculate the new opacity base on the scroll position */
+            const scrollPosition = Math.max(1 - scrollTop / maxScroll, 0.5); // mim opacity of 0.5
+            const scrollPositionStr = scrollPosition.toString();
+            if (parseInt(scrollPositionStr, 10) < 1) {
+                colorItem.style.background = `rgba(255, 255, 255, ${scrollPositionStr})`;
+            }
+            else {
+                colorItem.style.background = `rgba(163, 206, 241, 1)`;
+            }
+        });
+    }
+    catch (error) {
+        console.log(`An error occurred while attempting to lower opacity for '${querySelector}': `, error);
+    }
+}
+lowerBackgroundColor('header');
