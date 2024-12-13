1b4436b0c96fd40da3442f1b13cdd708baa9e665
ZachMuzzle <ZachMuzzle@users.noreply.github.com>
2024-12-13 01:05:37 +0000

    deploy: eb15107a6d5dee15312217a0dfa06895ba9a8192

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const path_1 = __importDefault(require("path"));
-exports.router = (0, express_1.Router)();
-exports.router.get('/', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, '../../public/index.html'));
+import { Router } from 'express';
+import path from 'path';
+export const router = Router();
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
