d475c5689ac963a3fac93fef9efcc7a8837d827f
ZachMuzzle <ZachMuzzle@users.noreply.github.com>
2024-12-23 21:59:29 +0000

    deploy: fbd9b76ceaddb41fcfb9b0d65f08be5ca355164a

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
 import { scrollPosition } from "./scrollPosition.js";
 class navBarToggle {
-    constructor(burgerId, navLinksId, navBarHomeId, navBarClass, sectionsWrapperClass) {
+    constructor(burgerId, navLinksId, navBarClass, sectionsWrapperClass) {
         this.buttonClick = false;
         this.burger = document.getElementById(burgerId);
         this.navBar = document.querySelector(navBarClass);
-        this.navBarHome = document.getElementById(navBarHomeId);
         this.navLinks = document.getElementById(navLinksId);
         this.sectionsWrapper = document.querySelector(sectionsWrapperClass);
         this.lastWidth = window.innerWidth;
-        this.lastHeight = window.innerHeight;
         // this.addEventListeners(); // For when method is private
     }
     toggleClasses() {
         this.burger.classList.toggle('active');
         this.navLinks.classList.toggle('show');
+        this.navBar.classList.toggle('visible');
+        this.sectionsWrapper.classList.toggle('addMarginLeft');
     }
     handleBurgerClick() {
         this.toggleClasses();
-        if (this.buttonClick) {
-            this.navBar.style.display = "none";
-            this.navBarHome.style.display = "flex";
-            this.sectionsWrapper.style.filter = 'none';
-        }
-        else {
-            this.navBar.style.display = "block";
-            this.navBarHome.style.display = "none";
-            const scrollPositionStr = scrollPosition();
-            if (parseInt(scrollPositionStr, 10) < 1) {
-                this.sectionsWrapper.style.filter = 'blur(3px)';
-            }
-        }
+        // if(this.buttonClick) {
+        //     this.navBar.style.display = "none";
+        //     this.sectionsWrapper.style.marginLeft = '0';
+        // } else {
+        //     this.navBar.style.display = "block";
+        //     this.sectionsWrapper.style.marginLeft = "150px";
+        //     // const scrollPositionStr = scrollPosition();
+        //     // if(parseInt(scrollPositionStr,10) < 1) {
+        //     //     this.sectionsWrapper.style.filter = 'blur(1px)';
+        //     // }
+        // }
         this.buttonClick = !this.buttonClick;
     }
     handleWindowResize() {
@@ -42,14 +40,14 @@ class navBarToggle {
         else if (this.burger.classList.contains('active')) {
             this.toggleClasses();
             this.navBar.style.display = "none";
+            this.sectionsWrapper.style.marginLeft = '0';
             this.buttonClick = false;
-            this.navBarHome.style.display = "flex";
         }
     }
     handleBlurWhenBurgerOpen() {
         const scrollPositionStr = scrollPosition();
         if (this.buttonClick && (parseInt(scrollPositionStr, 10) < 1)) {
-            this.sectionsWrapper.style.filter = 'blur(3px)';
+            this.sectionsWrapper.style.filter = 'blur(1px)';
         }
         else if (this.buttonClick && (parseInt(scrollPositionStr, 10) == 1)) {
             this.sectionsWrapper.style.filter = 'none';
@@ -58,8 +56,8 @@ class navBarToggle {
     addEventListeners() {
         this.burger.addEventListener('click', () => this.handleBurgerClick());
         window.addEventListener('resize', () => this.handleWindowResize());
-        window.addEventListener('scroll', () => this.handleBlurWhenBurgerOpen());
+        // window.addEventListener('scroll', () => this.handleBlurWhenBurgerOpen());
     }
 }
-const navToggle = new navBarToggle("burger", "nav-links", "nav-bar-home", ".navbar", ".sectionsWrapper");
+const navToggle = new navBarToggle("burger", "nav-links-left", ".mobileLeftMenu", ".sectionsWrapper");
 navToggle.addEventListeners();
--- ts/script.ts
+++ ts/script.ts
@@ -4,21 +4,17 @@ class navBarToggle {
     private burger: HTMLElement;
     private navLinks: HTMLElement;
     private navBar: HTMLElement;
-    private navBarHome: HTMLElement;
     private sectionsWrapper: HTMLElement;
     private lastWidth: number;
-    private lastHeight: number;
 
 
-    constructor(burgerId: string, navLinksId: string, navBarHomeId: string, navBarClass: string, sectionsWrapperClass: string) {
+    constructor(burgerId: string, navLinksId: string, navBarClass: string, sectionsWrapperClass: string) {
         this.buttonClick = false;
         this.burger = document.getElementById(burgerId) as HTMLElement;
         this.navBar = document.querySelector(navBarClass) as HTMLElement;
-        this.navBarHome = document.getElementById(navBarHomeId) as HTMLElement;
         this.navLinks = document.getElementById(navLinksId) as HTMLElement;
         this.sectionsWrapper = document.querySelector(sectionsWrapperClass) as HTMLElement
         this.lastWidth = window.innerWidth;
-        this.lastHeight = window.innerHeight;
 
 
         // this.addEventListeners(); // For when method is private
@@ -27,26 +23,27 @@ class navBarToggle {
     private toggleClasses(): void {
         this.burger.classList.toggle('active');
         this.navLinks.classList.toggle('show');
+        this.navBar.classList.toggle('visible');
+        this.sectionsWrapper.classList.toggle('addMarginLeft');
     }
 
     private handleBurgerClick(): void {
         this.toggleClasses();
 
-        if(this.buttonClick) {
-            this.navBar.style.display = "none";
-            this.navBarHome.style.display = "flex";
-            this.sectionsWrapper.style.filter = 'none';
+        // if(this.buttonClick) {
+        //     this.navBar.style.display = "none";
+        //     this.sectionsWrapper.style.marginLeft = '0';
 
             
-        } else {
-            this.navBar.style.display = "block";
-            this.navBarHome.style.display = "none";
-            const scrollPositionStr = scrollPosition();
+        // } else {
+        //     this.navBar.style.display = "block";
+        //     this.sectionsWrapper.style.marginLeft = "150px";
+        //     // const scrollPositionStr = scrollPosition();
 
-            if(parseInt(scrollPositionStr,10) < 1) {
-                this.sectionsWrapper.style.filter = 'blur(3px)';
-            }
-        }
+        //     // if(parseInt(scrollPositionStr,10) < 1) {
+        //     //     this.sectionsWrapper.style.filter = 'blur(1px)';
+        //     // }
+        // }
 
         this.buttonClick = !this.buttonClick;
     }
@@ -60,8 +57,8 @@ class navBarToggle {
         else if(this.burger.classList.contains('active')) {
             this.toggleClasses();
             this.navBar.style.display = "none";
+            this.sectionsWrapper.style.marginLeft = '0';
             this.buttonClick = false;
-            this.navBarHome.style.display = "flex";
         }
     }
 
@@ -69,7 +66,7 @@ class navBarToggle {
         const scrollPositionStr = scrollPosition();
         if(this.buttonClick && (parseInt(scrollPositionStr,10) < 1)) {
 
-            this.sectionsWrapper.style.filter = 'blur(3px)';
+            this.sectionsWrapper.style.filter = 'blur(1px)';
             
         } else if (this.buttonClick && (parseInt(scrollPositionStr,10) == 1)) {
             this.sectionsWrapper.style.filter = 'none';
@@ -79,9 +76,9 @@ class navBarToggle {
     public addEventListeners(): void {
         this.burger.addEventListener('click', () => this.handleBurgerClick());
         window.addEventListener('resize', () => this.handleWindowResize());
-        window.addEventListener('scroll', () => this.handleBlurWhenBurgerOpen());
+        // window.addEventListener('scroll', () => this.handleBlurWhenBurgerOpen());
     }
 }
 
-const navToggle = new navBarToggle("burger","nav-links","nav-bar-home",".navbar",".sectionsWrapper");
+const navToggle = new navBarToggle("burger","nav-links-left",".mobileLeftMenu",".sectionsWrapper");
 navToggle.addEventListeners();
